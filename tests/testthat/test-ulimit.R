4423bab31517f95fc354cd95124fa90bc7ee0def
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-04-15 00:45:27 +0200

    full coverage

--- tests/testthat/test-ulimit.R
+++ tests/testthat/test-ulimit.R
@@ -9,9 +9,9 @@ test_that("Memory limits can be read", {
 test_that("Memory limits can be set", {
   l <- memory_limit()
   on.exit(memory_limit(l[["soft"]]), add = TRUE)
-  memory_limit(256)
+  memory_limit(1)
   new_l <- memory_limit()
-  expect_equal(new_l[["soft"]], 256)
+  expect_equal(new_l[["soft"]], 1)
   expect_equal(new_l[["hard"]], l[["hard"]])
 })
 
@@ -22,4 +22,16 @@ test_that("Memory limits are respected", {
   memory_limit(256)
   expect_equal(length(1:100000), 100000)
   expect_error(length(1:100000000), "allocate")
+})
+
+test_that("Memory limits below zero and infinity can be set", {
+  skip_on_cran()
+  l <- memory_limit()
+  on.exit(memory_limit(l[["soft"]]), add = TRUE)
+  memory_limit(-10)
+  new_l <- memory_limit()
+  expect_equal(new_l[["soft"]], 0)
+  memory_limit(Inf)
+  new_l <- memory_limit()
+  expect_equal(new_l[["soft"]], Inf)
 })
